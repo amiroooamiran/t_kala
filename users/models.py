34e416bd37ac8cc5e60aa0b0ea1b65cb65ee9bd4
amiroooamiran <amiran.amirhossein@gmail.com>
2024-01-30 17:33:22 +0330

    create base of the models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class account(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    name = models.CharField(max_length=300, blank=True)
+    last_name = models.CharField(max_length=300, blank=True)
+    avatar = models.ImageField(upload_to='user_profile/', default='avatar.jpg', blank=True)
+    adress = models.TextField(blank=True)
+    post_code = models.CharField(max_length=9, blank=True)
+
+    def __str__(self):
+        return self.user.username
